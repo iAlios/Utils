47cd3e3988165f564175b711b8f5acb17b5592d1
i.lufei <dzxsk1988@gmail.com>
2015-04-10 16:04:45 +0800

    添加 short 相关的处理

--- com/alio/utils/IntUtils.java
+++ com/alio/utils/IntUtils.java
@@ -4,6 +4,68 @@ package com.alio.utils;
 public class IntUtils {
 
 	/**
+	 * short 转 byte[] (由高位到低位)
+	 * 
+	 * @param i
+	 * @return
+	 */
+	public static byte[] shortToByteArray(short i) {
+		byte[] result = new byte[2];
+		// 由高位到低位
+		result[0] = (byte) ((i >> 8) & 0xFF);
+		result[1] = (byte) (i & 0xFF);
+		return result;
+	}
+
+	/**
+	 * byte[]转 short (由高位到低位)
+	 * 
+	 * @param bytes
+	 * @return
+	 */
+	public static short byteArrayToShort(byte[] bytes) {
+		short value = 0;
+		int shift = 0;
+		// 由高位到低位
+		for (int i = 0; i < 2; i++) {
+			shift = (2 - 1 - i) * 8;
+			value += (bytes[i] & 0x000000FF) << shift;// 往高位游
+		}
+		return value;
+	}
+
+	/**
+	 * short 转 byte[](由低位到高位)
+	 * 
+	 * @param i
+	 * @return
+	 */
+	public static byte[] shortToByteArrayL(short i) {
+		byte[] result = new byte[2];
+		// 由低位到高位
+		result[1] = (byte) ((i >> 8) & 0xFF);
+		result[0] = (byte) (i & 0xFF);
+		return result;
+	}
+
+	/**
+	 * byte[]转  short (由低位到高位)
+	 * 
+	 * @param bytes
+	 * @return
+	 */
+	public static int byteArrayToShortL(byte[] bytes) {
+		int value = 0;
+		// 由低位到高位
+		int shift = 0;
+		for (int i = 0; i < 2; i++) {
+			shift = i * 8;
+			value += (bytes[i] & 0x000000FF) << shift;// 往高位游
+		}
+		return value;
+	}
+
+	/**
 	 * int转 byte[] (由高位到低位)
 	 * 
 	 * @param i
@@ -70,11 +132,17 @@ public class IntUtils {
 	}
 
 	public static void main(String[] args) {
-		int dd = 2;
+		short dd = 2;
+		System.out.println(HexDump.dumpHexString(shortToByteArray(dd)));
+		System.out.println(byteArrayToShort(shortToByteArray(dd)));
+		System.out.println(HexDump.dumpHexString(shortToByteArrayL(dd)));
+		System.out.println(byteArrayToShortL(shortToByteArrayL(dd)));
+
 		System.out.println(HexDump.dumpHexString(intToByteArray(dd)));
 		System.out.println(byteArrayToInt(intToByteArray(dd)));
 		System.out.println(HexDump.dumpHexString(intToByteArrayL(dd)));
 		System.out.println(byteArrayToIntL(intToByteArrayL(dd)));
+
 	}
 
 }
