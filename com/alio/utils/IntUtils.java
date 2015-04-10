1daa8b3d8a82fb8535a2f2177e08c564b2ec78c1
i.lufei <dzxsk1988@gmail.com>
2015-04-10 16:11:45 +0800

    添加 浮点数 转换

--- com/alio/utils/IntUtils.java
+++ com/alio/utils/IntUtils.java
@@ -49,7 +49,7 @@ public class IntUtils {
 	}
 
 	/**
-	 * byte[]转  short (由低位到高位)
+	 * byte[]转 short (由低位到高位)
 	 * 
 	 * @param bytes
 	 * @return
@@ -131,6 +131,46 @@ public class IntUtils {
 		return value;
 	}
 
+	/**
+	 * 浮点转换为字节
+	 * 
+	 * @param f
+	 * @return
+	 */
+	public static byte[] float2byte(float f) {
+		// 把float转换为byte[]
+		int fbit = Float.floatToIntBits(f);
+		byte[] b = new byte[4];
+		for (int i = 0; i < 4; i++) {
+			b[i] = (byte) (fbit >> (24 - i * 8));
+		}
+		// 翻转数组
+		int len = b.length;
+		// 建立一个与源数组元素类型相同的数组
+		byte[] dest = new byte[len];
+		// 为了防止修改源数组，将源数组拷贝一份副本
+		System.arraycopy(b, 0, dest, 0, len);
+		byte temp;
+		// 将顺位第i个与倒数第i个交换
+		for (int i = 0; i < len / 2; ++i) {
+			temp = dest[i];
+			dest[i] = dest[len - i - 1];
+			dest[len - i - 1] = temp;
+		}
+		return dest;
+	}
+
+	public static float byteArrayToFloat(byte[] b) {
+		int l = b[0];
+		l &= 0xff;
+		l |= ((long) b[1] << 8);
+		l &= 0xffff;
+		l |= ((long) b[2] << 16);
+		l &= 0xffffff;
+		l |= ((long) b[3] << 24);
+		return Float.intBitsToFloat(l);
+	}
+
 	public static void main(String[] args) {
 		short dd = 2;
 		System.out.println(HexDump.dumpHexString(shortToByteArray(dd)));
@@ -143,6 +183,8 @@ public class IntUtils {
 		System.out.println(HexDump.dumpHexString(intToByteArrayL(dd)));
 		System.out.println(byteArrayToIntL(intToByteArrayL(dd)));
 
+		System.out.println(HexDump.dumpHexString(float2byte(dd * 0.1f)));
+		System.out.println(byteArrayToFloat(float2byte(dd * 0.1f)));
 	}
 
 }
